979e6f9a475bc77fa3f5893c93d69bed3b94e066
olibraun <oliverbraun89@gmail.com>
2017-06-03 18:25:09 +0200

    SERIOUS ERROR FIXED: Fix creation of matingpool.

--- smartrockets/sketch.js
+++ smartrockets/sketch.js
@@ -40,19 +40,19 @@ function Population(){
   this.evaluate = function(){
     console.log("evaluate");
     var maxfit = 0;
-    for(var i=0; i< this.popsize; i++){
+    for(var i=0; i < this.popsize; i++){
       this.rockets[i].calcFitness();
       if(this.rockets[i].fitness > maxfit){
         maxfit = this.rockets[i].fitness;
       }
     }
-    for(var i=0; i< this.popsize; i++){
+    for(var i=0; i < this.popsize; i++){
       this.rockets[i].fitness /= maxfit;
     }
     this.matingpool = [];
-    for(var i=0; i< this.popsize; i++){
+    for(var i=0; i < this.popsize; i++){
       var n = this.rockets[i].fitness * 100;
-      for(var j=0; j< this.popsize; j++){
+      for(var j=0; j < n; j++){
         this.matingpool.push(this.rockets[i]);
       }
     }
