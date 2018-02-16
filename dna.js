92ee5046cb1af11bcbe3c76a632b84a3b2c21f20
olibraun <oliverbraun89@gmail.com>
2018-02-16 21:26:52 +0100

    refactor: Migrate DNA into an ES6 class.

--- dna.js
+++ dna.js
@@ -1,30 +1,35 @@
-function DNA(genes){
-  if(genes){
-    this.genes = genes;
-  }else{
-    this.genes = [];
-    for(var i=0; i<lifespan; i++){
-      this.genes[i] = p5.Vector.random2D();
-      this.genes[i].setMag(maxforce);
+class DNA {
+  constructor(genes) {
+    // Conditional constructor which should work without an argument
+    if(genes) {
+      // empty argument case
+      this.genes = genes;
+    } else {
+      // argument is defined
+      this.genes = [];
+      for(let i=0; i < lifespan; i++) {
+        this.genes[i] = p5.Vector.random2D();
+        this.genes[i].setMag(maxforce);
+      }
     }
   }
 
-  this.crossover = function(partner){
-    var newgenes = [];
-    var mid = floor(random(this.genes.length));
-    for(var i=0; i<this.genes.length; i++){
-      if(i>mid){
+  crossover(partner) {
+    let newgenes = [];
+    let mid = floor(random(this.genes.length));
+    for(let i=0; i<this.genes.length; i++) {
+      if(i>mid) {
         newgenes[i] = this.genes[i];
-      }else{
+      } else {
         newgenes[i] = partner.genes[i];
       }
     }
     return new DNA(newgenes);
   }
 
-  this.mutation = function(){
-    for(var i=0; i<this.genes.length; i++){
-      if(random(1) < 0.01){
+  mutation() {
+    for(let i=0; i<this.genes.length; i++) {
+      if(random(1) < 0.01) {
         this.genes[i] = p5.Vector.random2D();
         this.genes[i].setMag(0.1);
       }
